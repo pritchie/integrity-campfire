829551b4dd477dfa38da9e234fdb77d2ba7d533d
Patrick Ritchie <pritchie@gmail.com>
2009-04-30 10:30:25 -0400

    update notifier to use new integrity attribute names

--- lib/notifier/campfire.rb
+++ lib/notifier/campfire.rb
@@ -12,30 +12,28 @@ module Integrity
       end
 
       def deliver!
-        room.speak "#{short_message}. #{commit_url}"
-        room.paste full_message if commit.failed?
+        room.speak "#{short_message}. #{build_url}"
+        room.paste full_message if build.failed?
       end
 
     private
       def room
         @room ||= begin
-          options = {}
-          options[:ssl] = config['use_ssl'] ? true : false
-          campfire = Tinder::Campfire.new(config['account'], options)
+          campfire = Tinder::Campfire.new(config['account'])
           campfire.login(config['user'], config['pass'])
           campfire.find_room_by_name(config['room'])
         end
       end
 
       def short_message
-        "Build #{commit.short_commit_identifier} of #{commit.project.name} #{commit.successful? ? "was successful" : "failed"}"
+        "Build #{build.short_identifier} of #{build.project.name} #{build.successful? ? "was successful" : "failed"}"
       end
 
       def full_message
         <<-EOM
-Commit Message: #{commit.commit_message}
-Commit Date: #{commit.commited_at}
-Commit Author: #{commit.commit_author.name}
+Commit Message: #{build.message}
+Commit Date: #{build.committed_at}
+Commit Author: #{build.author.name}
 
 #{stripped_build_output}
 EOM
